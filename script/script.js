23d2d863c8de6b851827cd9ec2a7b045a8395fda
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-09-14 16:11:16 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,39 @@
+
+const createShedule=function()
+{
+    const createShedule1=document.getElementById("schedule");
+    for(let i=0;i<20;i++)
+    {
+        const numberCell=document.createElement("div");
+            numberCell.classList.add("system");
+            const numbervalue=document.createElement("h3");
+            numbervalue.innerText=Math.floor(Math.random()*100);
+            numberCell.appendChild(numbervalue);
+            createShedule1.appendChild(numberCell);
+            numberCell.addEventListener("click", function(e)
+            {
+                e.preventDefault()
+                numberCell.classList.add("cellselected");
+                
+            })
+    }
+    const deletes=document.createElement("div");
+    deletes.innerHTML=`<button onclick="svuotatutto(event)">svuota scheda</button>`
+    createShedule1.appendChild(deletes);
+   
+   
+    
+
+}
+const svuotatutto=function(e)
+{
+    e.preventDefault();
+    const everytab=document.getElementsByClassName("system");
+    const everytab1=Array.from(everytab);
+    for (let i=0;i<everytab1.length;i++)
+    {
+        everytab1[i].classList.remove("cellselected");
+    }
+}
+
+createShedule();
